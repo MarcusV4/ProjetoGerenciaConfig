e919063159f042a5289f900cea0b60bcf9acb9de
LKS-16 <araujolukas093@gmail.com>
2025-10-05 20:07:49 -0300

    pull da develop

--- src/Soma.java
+++ src/Soma.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
+public class Soma implements Operacao {
 
-public class Soma {
+    @Override
+    public double calcular(double a, double b) {
+        return a + b;
+    }
 }
