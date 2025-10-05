bd7a96f6f1569d1a20d14049653e7a05235cbd33
Marcus <mvc7730@gmail.com>
2025-10-05 20:57:07 -0300

    feat: Função client

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+
+        Contexto contexto = new Contexto();
+        contexto.setOperacao(new Soma());
+        System.out.println(contexto.calcular(2, 5));
+    }
 }
