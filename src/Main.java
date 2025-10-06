73cb4acb416650c7ad4a88af8e2fcb79d4bb6c2f
Marcus <mvc7730@gmail.com>
2025-10-05 21:21:06 -0300

    Corrigindo operação errada

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         Contexto contexto = new Contexto();
-        contexto.setOperacao(new Soma());
+        contexto.setOperacao(new Multiplicacao());
         System.out.println("Resultado da operação é = "+contexto.calcular(2, 5));
     }
 }
